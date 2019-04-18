5c39dc8e8ccefac4222c5d8d98aa6c3a6690129c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-04-18 22:33:25 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw 
+resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'post/:id', to: 'posts#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
