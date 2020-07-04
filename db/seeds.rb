cbfd3ff49188e75d5324e555f30a9270b3a04cad
bruffin1221 <bruffin1221@gmail.com>
2020-07-04 08:39:42 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title:"Getting Big", description:"Think Like A Millionaire")
+Post.create(title:"Vision", description:"Start Small an Build")
+Post.create(title:"About the Money", description:"Building An Income")
+Post.create(title:"One to Many", description:"Adding Extra Streams")
